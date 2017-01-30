0dd9638824083488eb6a0f865ebb351ce618bb66
Priest4612 <prie4612@gmail.com>
2017-01-30 16:34:59 +0300

    Добавлена функция рисования текста, добавлена функция рандомного цвета

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
-  var drowCloud = function (ctx, x, y, width, height, roundoff, offset){
+  var drowCloud = function (x, y, width, height, roundoff, offset){
     ctx.beginPath();
     ctx.moveTo(x + roundoff + offset, y + offset);
     ctx.lineTo(x + width - roundoff + offset, y + offset); //1 линия
@@ -14,10 +14,23 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.closePath();
   };
 
-  var drowHisto = function(ctx, names, times) {
-    function getMaxOfArray(numArray) {
+  var drowMessage = function (message, color, fontSize, font,  x, y ) {
+    ctx.fillStyle = color;
+    ctx.font = [fontSize, 'px ', font].join('');
+    ctx.fillText(message, x, y);
+  };
+
+  var drowHisto = function (names, times) {
+    function getMaxOfArray (numArray) {
       return Math.max.apply(null, numArray);
     }
+
+    function randomColor() {
+      var rnd = (Math.random() * 255).toFixed(0);
+      var alpha = (0.5 + Math.random() * (1 + 0.5)).toFixed(1);
+      return ['rgba(0, 0, ', rnd, ', ', alpha, ')'].join('');
+    }
+
     var histoHeight = 150;
     var histoX = 140;
     var startColumn = 250;
@@ -33,34 +46,26 @@ window.renderStatistics = function (ctx, names, times) {
       if(name === 'Вы'){
         ctx.fillStyle = 'rgba(255, 0, 0, 1)';
       } else {
-        ctx.fillStyle = ['rgba(0, 0, ', ((Math.random() * 5) * 50).toFixed(0), ', ', (0.5 + Math.random() * (1 + 0.5)).toFixed(1), ')'].join('');
+        ctx.fillStyle = randomColor();
       }
       ctx.fillRect(histoX + columnIndent * i, startColumn, widthColumn, -height);
       ctx.fillText(name, histoX + columnIndent * i, startColumn + 20);
       ctx.fillText(time.toFixed(0), histoX + columnIndent * i, startColumn - height - 5);
     }
-  }
+  };
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  drowCloud(ctx, 100, 10, 420, 270, 30, 10);
+  drowCloud(100, 10, 420, 270, 30, 10);
   ctx.fill();
 
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-  drowCloud(ctx, 100, 10, 420, 270, 30, 0);
+  drowCloud(100, 10, 420, 270, 30, 0);
   ctx.fill();
   ctx.strokeStyle = 'blue';
   ctx.lineWidth = 5;
   ctx.stroke();
 
-  ctx.fillStyle = '#000000';
-  ctx.font = '24px PT Mono';
-  ctx.fillText('Ура вы победили!', 210, 40);
-
-  ctx.fillStyle = '#000000';
-  ctx.font = '18px PT Mono';
-  ctx.fillText('Список результатов:', 130, 65);
-  drowHisto(ctx, names, times);
+  drowMessage('Ура вы победили!', '#000000', 24, 'PT Mono', 210, 40);
+  drowMessage('Список результатов:','#000000',  18, 'PT Mono', 130, 65);
+  drowHisto(names, times);
 };
-
-
-
