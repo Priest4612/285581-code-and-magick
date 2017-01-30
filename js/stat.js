e4d122677206b3399f7c48a92fa5ac8730ef8906
Priest4612 <prie4612@gmail.con>
2017-01-30 22:27:22 +0400

    Внесены изменения. Код проходит проверку eslint

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,11 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
-  var drowCloud = function (x, y, width, height, roundoff, offset){
+  var drawCloud = function (x, y, width, height, roundoff, offset) {
     ctx.beginPath();
     ctx.moveTo(x + roundoff + offset, y + offset);
-    ctx.lineTo(x + width - roundoff + offset, y + offset); //1 линия
-    ctx.quadraticCurveTo(x + width + offset, y + offset, x + width + offset, y + roundoff + offset); //скругление в п
-    ctx.lineTo(x + width + offset, y + height - roundoff + offset); //2 линия впу к нпу
+    ctx.lineTo(x + width - roundoff + offset, y + offset);
+    ctx.quadraticCurveTo(x + width + offset, y + offset, x + width + offset, y + roundoff + offset);
+    ctx.lineTo(x + width + offset, y + height - roundoff + offset);
     ctx.quadraticCurveTo(x + width + offset, y + height + offset, x + width - roundoff + offset, y + height + offset);
     ctx.lineTo(x + roundoff + offset, y + height + offset);
     ctx.quadraticCurveTo(x + offset, y + height + offset, x + offset, y + height - roundoff + offset);
@@ -14,22 +14,25 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.closePath();
   };
 
-  var drowMessage = function (message, color, fontSize, font,  x, y ) {
+  var drawMessage = function (message, color, fontSize, font, x, y) {
     ctx.fillStyle = color;
     ctx.font = [fontSize, 'px ', font].join('');
     ctx.fillText(message, x, y);
   };
 
-  var drowHisto = function (names, times) {
-    function getMaxOfArray (numArray) {
+  var drawHisto = function () {
+    var getMaxOfArray = function (numArray) {
       return Math.max.apply(null, numArray);
-    }
+    };
 
-    function randomColor() {
-      var rnd = ((Math.random() * 5) * 50).toFixed(0);
-      var alpha = (0.5 + Math.random() * (1 - 0.5)).toFixed(1);
-      return ['rgba(0, 0, ', rnd, ', ', alpha, ')'].join('');
-    }
+    var drawRect = function (x1, y1, x2, y2, color) {
+      ctx.fillStyle = color || '#000000';
+      ctx.fillRect(x1, y1, x2, y2);
+    };
+
+    var randomColor = function () {
+      return ['rgba(0, 0, ', (Math.random() * 255).toFixed(0), ', ', (0.5 + Math.random() * (1 - 0.5)).toFixed(1), ')'].join('');
+    };
 
     var histoHeight = 150;
     var histoX = 140;
@@ -37,35 +40,36 @@ window.renderStatistics = function (ctx, names, times) {
     var widthColumn = 40;
     var columnIndent = widthColumn + 50;
     var step = histoHeight / getMaxOfArray(times);
+    var correntColor;
 
     for (var i = 0; i < times.length; i++) {
       var name = names[i];
-      var time = times[i];
-      var height = step * time;
+      var height = step * times[i];
+      var indent = histoX + columnIndent * i;
 
-      if(name === 'Вы'){
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      if (name === 'Вы') {
+        correntColor = 'rgba(255, 0, 0, 1)';
       } else {
-        ctx.fillStyle = randomColor();
+        correntColor = randomColor();
       }
-      ctx.fillRect(histoX + columnIndent * i, startColumn, widthColumn, -height);
-      ctx.fillText(name, histoX + columnIndent * i, startColumn + 20);
-      ctx.fillText(time.toFixed(0), histoX + columnIndent * i, startColumn - height - 5);
+      drawRect(indent, startColumn, widthColumn, -height, correntColor);
+      ctx.fillText(name, indent, startColumn + 20);
+      ctx.fillText(times[i].toFixed(0), indent, startColumn - height - 5);
     }
   };
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  drowCloud(100, 10, 420, 270, 30, 10);
+  drawCloud(100, 10, 420, 270, 30, 10);
   ctx.fill();
 
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-  drowCloud(100, 10, 420, 270, 30, 0);
+  drawCloud(100, 10, 420, 270, 30, 0);
   ctx.fill();
   ctx.strokeStyle = 'blue';
   ctx.lineWidth = 5;
   ctx.stroke();
 
-  drowMessage('Ура вы победили!', '#000000', 24, 'PT Mono', 210, 40);
-  drowMessage('Список результатов:','#000000',  18, 'PT Mono', 130, 65);
-  drowHisto(names, times);
+  drawMessage('Ура вы победили!', '#000000', 24, 'PT Mono', 210, 40);
+  drawMessage('Список результатов:', '#000000', 18, 'PT Mono', 130, 65);
+  drawHisto(names, times);
 };
